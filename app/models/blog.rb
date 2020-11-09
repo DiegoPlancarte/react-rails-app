e115befa28e817e38f38a8e8ea3e255c524b1b8a
Diego Plancarte <dplancarte7@gmail.com>
2020-11-09 11:44:49 -0800

    Updated user and blog model for favorting roles. Ran db migration.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,4 @@
 class Blog < ApplicationRecord
   belongs_to :user
+  acts_as_favoritable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable
   has_many :blogs
   has_many :projects
+  acts_as_favoritor
 end
